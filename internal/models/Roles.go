b06859d24014f0948e87cfdf668cb49abdeeb238
Native onGfuel <nativeongfuel@MacBook-Pro-232.local>
2025-12-02 00:31:29 +0100

    new changes

--- internal/models/Roles.go
+++ internal/models/Roles.go
@@ -1,9 +1,46 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"LindaBen_Phase_1_Project/internal/db"
+
+	"gorm.io/gorm"
+)
 
 type Role struct {
 	gorm.Model
 	RoleName    string       `gorm:"uniqueIndex"`
 	Permissions []Permission `gorm:"many2many:role_permissions;"`
+}
+
+// Create a role
+func CreateRole(Role *Role) (err error) {
+	err = db.Create(Role).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get all roles
+func GetRoles(Role *[]Role) (err error) {
+	err = db.Find(Role).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get role by id
+func GetRole(Role *Role, id int) (err error) {
+	err = db.Where("id = ?", id).First(Role).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update role
+func UpdateRole(Role *Role) (err error) {
+	db.Save(Role)
+	return nil
 }
--- internal/models/User.go
+++ internal/models/User.go
@@ -1,6 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"LindaBen_Phase_1_Project/internal/db"
+	"html"
+	"strings"
+
+	"gorm.io/gorm"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	gorm.Model
@@ -12,4 +20,76 @@ type User struct {
 
 	AvatarID *uint
 	Avatar   *File `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+// Save user details
+func (user *User) Save() (*User, error) {
+	err := db.Create(&user).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return user, nil
+}
+
+// Generate encrypted password
+func (user *User) BeforeSave(*gorm.DB) error {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(passwordHash)
+	user.Username = html.EscapeString(strings.TrimSpace(user.Username))
+	return nil
+}
+
+// Get all users
+func GetUsers(User *[]User) (err error) {
+	err = database.Db.Find(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get user by username
+func GetUserByUsername(username string) (User, error) {
+	var user User
+	err := database.Db.Where("username=?", username).Find(&user).Error
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+// Validate user password
+func (user *User) ValidateUserPassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+}
+
+// Get user by id
+func GetUserById(id uint) (User, error) {
+	var user User
+	err := database.Db.Where("id=?", id).Find(&user).Error
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+// Get user by id
+func GetUser(User *User, id int) (err error) {
+	err = database.Db.Where("id = ?", id).First(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update user
+func UpdateUser(User *User) (err error) {
+	err = database.Db.Omit("password").Updates(User).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
