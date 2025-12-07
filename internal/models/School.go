253c09c329c1a68da738689a3e9ab97d7ff32f63
Native onGfuel <nativeongfuel@MacBook-Pro-232.local>
2025-12-07 22:20:07 +0100

    DB Changes + New API

--- internal/models/School.go
+++ internal/models/School.go
@@ -1,15 +1,62 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"LindaBen_Phase_1_Project/internal/db"
+
+	"gorm.io/gorm"
+)
 
 type School struct {
-	gorm.Model
-	Name    string
-	Address string
-	Lat     float64
-	Lon     float64
+	gorm.Model // includes ID, CreatedAt, UpdatedAt, DeletedAt
+	Name       string
+	Address    string
+	Location   float64 // e.g. GPS coordinates
 
 	ContactID *uint
-	Contact   *User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Contact   *User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` //Belongs to User
 }
 
+// Get all Schools
+func GetAllSchools(School *[]School) (err error) {
+	err = db.Db.Find(School).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get School by ID
+func GetSchoolByID(School *School, id uint) (err error) {
+	err = db.Db.First(School, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Create School
+func CreateSchool(School *School) (err error) {
+	err = db.Db.Create(School).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Update School
+func UpdateSchool(School *School) (err error) {
+	err = db.Db.Save(School).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete School
+func DeleteSchool(School *School) (err error) {
+	err = db.Db.Delete(School).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
