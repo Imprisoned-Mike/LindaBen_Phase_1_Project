78404b4f473a1817121f97892105b9f351f06f02
beavan-james <173955023+beavan-james@users.noreply.github.com>
2025-12-09 12:41:02 +0100

    Error Fixes

--- internal/db/database.go
+++ internal/db/database.go
@@ -1,9 +1,7 @@
 package db
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -12,27 +10,13 @@ import (
 var Db *gorm.DB
 
 func InitDb() *gorm.DB {
-	Db = connectDB()
-	return Db
-}
-
-func connectDB() *gorm.DB {
 	var err error
-	host := os.Getenv("DB_HOST")
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, dbname)
-	//log.Println("dsn : ", dsn)
-	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
-
+	Db, err = gorm.Open(sqlite.Open("mydatabase.db"), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Error connecting to database :", err)
+		log.Fatal("Error connecting to database:", err)
 		return nil
 	}
-	log.Println("Successfully connected to the database")
 
-	return db
+	log.Println("Successfully connected to the database")
+	return Db
 }
