1a4dc63dc5a2789916104767dab4ef06d036eb39
Imprisoned-Mike <173955023+Imprisoned-Mike@users.noreply.github.com>
2025-12-14 00:44:16 -0500

    Avatar

--- internal/models/File.go
+++ internal/models/File.go
@@ -1,9 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"LindaBen_Phase_1_Project/internal/db"
+
+	"gorm.io/gorm"
+)
 
 type File struct {
 	gorm.Model
-	ID  uint
 	Url string
 }
+
+// Save File details
+func (file *File) Save() (*File, error) {
+	if err := db.Db.Create(file).Error; err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
